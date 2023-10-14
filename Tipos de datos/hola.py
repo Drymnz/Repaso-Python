ddc0cb76909eb5e2eee7fe84e0105ec4620eaaca
Drymnz <85306742+Drymnz@users.noreply.github.com>
2023-10-14 17:14:55 -0600

    primer commt

---
+++ Tipos de datos/hola.py
@@ -0,0 +1,20 @@
+print (" Hola mundo") 
+
+#tipos de datos solo una linea de texto
+'hola mundo '
+"Hola mundo"
+
+#varios lineas de texto
+'''hola mundo 
+de varias lineas de 
+esta forma infinita'''
+
+""" EStas de puede usar en vairas lineas 
+de codigo y no secuencia """
+
+# numeros
+40 
+4.5
+True 
+False
+
---
+++ Vairables/Variables.py
@@ -0,0 +1,32 @@
+# lo comun de todo lenguaje
+
+nombre = "Benjamin  Jesus Perez Aguilar"
+nombre = 'Benjamin De Jesus Perez Aguilar'
+
+print (nombre)
+
+# unir palabras
+
+gungar = 'El siguiente nombre es un nombre'
+todo_Unido =  gungar + nombre
+
+print ( gungar + nombre)
+
+otra_forma = 'El siguiente nombre es un nombre {nombre}'
+print (otra_forma)
+
+#concuidado con el f 
+# se conoce con f string
+otra_forma = f'El siguiente nombre es un nombre {nombre}'
+
+print (otra_forma)
+
+### interesante esto
+otra_forma_1 = otra_forma + ' se aguardo en la base de datos'
+
+# borrar de la memoria 
+del otra_forma
+
+print (otra_forma_1)
+
+
